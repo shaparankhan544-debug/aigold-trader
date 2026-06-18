dcc5344a8afbd0b3b5ada4b5d543471ea5f922cd
SHAPARAN KHAN <shaparankhan544@gmail.com>
2026-06-18 18:11:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function buySignal() {
+  document.getElementById("result").innerHTML = "BUY Signal";
+}
 
+function sellSignal() {
+  document.getElementById("result").innerHTML = "SELL Signal";
+}
